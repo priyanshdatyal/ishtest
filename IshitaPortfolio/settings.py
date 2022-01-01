902a3b04f982cebec951646e0f5286b2a7cac762
Priyansh Datyal <73377519+priyanshdatyal@users.noreply.github.com>
2022-01-02 04:15:49 +0530

    Update settings.py

--- IshitaPortfolio/settings.py
+++ IshitaPortfolio/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
