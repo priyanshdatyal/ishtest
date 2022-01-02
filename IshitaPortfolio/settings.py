85e237e0a3480d7f1e6bf23ee02fe924d0f450ed
Priyansh Datyal <73377519+priyanshdatyal@users.noreply.github.com>
2022-01-02 06:19:42 +0530

    Update settings.py

--- IshitaPortfolio/settings.py
+++ IshitaPortfolio/settings.py
@@ -123,9 +123,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
